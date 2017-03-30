f8e4edc4f0a443a41104ba40e930fa08239b9415
John Purchase <john.w.e.purchase@gmail.com>
2017-03-30 15:49:28 -0400

    added vue to editing clientlib group - no more ajax load

--- ui.apps/src/main/content/jcr_root/apps/breadboard/components/structure/rowComponent/dialog/clientlib/rowEditor.js
+++ ui.apps/src/main/content/jcr_root/apps/breadboard/components/structure/rowComponent/dialog/clientlib/rowEditor.js
@@ -4,93 +4,84 @@
     $document.on("dialog-ready", function(){
 
 
+            var componentPath = $('#rowDlg').attr("data-component-path");
+            $.get(componentPath + '.infinity.json', function(data){
 
+                var columns = [];
 
-            $.getScript('/etc/designs/breadboard/clientlib-site/vue.js')
-                .done(function( script, textStatus ) {
+                _.keys(data).forEach(function(key){
+                    if(key.startsWith("col-")) {
+                        var column = {
+                            data: data[key],
+                            name: key,
+                            active: false
+                        };
+                        columns.push(column);
+                    }
+                });
 
-                    var componentPath = $('#rowDlg').attr("data-component-path");
-                    $.get(componentPath + '.infinity.json', function(data){
+                columns[0].active = true;
 
-                        var columns = [];
-
-                        _.keys(data).forEach(function(key){
-                            if(key.startsWith("col-")) {
-                                var column = {
-                                    data: data[key],
-                                    name: key,
-                                    active: false
-                                };
-                                columns.push(column);
-                            }
-                        });
-
-                        columns[0].active = true;
-
-                        var dlg = new Vue({
-                            el: '#rowDlg',
-                            data: {
-                                columns: columns,
-                                raw: data
-                            },
-                            methods: {
-                                clickTab: function(index) {
-                                    for (var i = 0 ; i < columns.length; i++) {
-                                        if(i != index) {
-                                            columns[i].active = false;
-                                        } else {
-                                            columns[i].active = true;
-                                        }
-                                    }
-                                },
-                                saveDialog: function(event) {
-                                    //event.preventDefault();
-
-                                    $.ajax({
-                                        type: "POST",
-                                        url: componentPath,
-                                        data: {
-                                            ':operation': 'import',
-                                            ':contentType': 'json',
-                                            ':content': JSON.stringify(dlg.raw),
-                                            ':replace': true,
-                                            ':replaceProperties': true
-                                        }
-
-                                    })
-                                        .done(function(msg){
-                                            console.log("data saved");
-                                        });
-                                },
-                                addColumn: function() {
-                                    var newColumn = {
-                                        data: {
-                                            'jcr:primaryType': 'nt:unstructured',
-                                            lrgWidth: 0,
-                                            lrgOffset: 0,
-                                            medWidth: 0,
-                                            medOffset: 0,
-                                            smlWidth: 0,
-                                            smlOffset: 0
-                                        },
-                                        name: 'col-'+new String(columns.length + 1),
-                                        active: false
-                                    }
-
-                                    columns.push(newColumn);
-                                    dlg.raw[newColumn.name] = newColumn.data;
-                                    dlg.methods.clickTab(columns.length - 1);
+                var dlg = new Vue({
+                    el: '#rowDlg',
+                    data: {
+                        columns: columns,
+                        raw: data
+                    },
+                    methods: {
+                        clickTab: function(index) {
+                            for (var i = 0 ; i < columns.length; i++) {
+                                if(i != index) {
+                                    columns[i].active = false;
+                                } else {
+                                    columns[i].active = true;
                                 }
                             }
-                        });
+                        },
+                        saveDialog: function(event) {
+                            //event.preventDefault();
 
+                            $.ajax({
+                                type: "POST",
+                                url: componentPath,
+                                data: {
+                                    ':operation': 'import',
+                                    ':contentType': 'json',
+                                    ':content': JSON.stringify(dlg.raw),
+                                    ':replace': true,
+                                    ':replaceProperties': true
+                                }
 
-                    });
+                            })
+                                .done(function(msg){
+                                    console.log("data saved");
+                                });
+                        },
+                        addColumn: function() {
+                            var newColumn = {
+                                data: {
+                                    'jcr:primaryType': 'nt:unstructured',
+                                    lrgWidth: 0,
+                                    lrgOffset: 0,
+                                    medWidth: 0,
+                                    medOffset: 0,
+                                    smlWidth: 0,
+                                    smlOffset: 0
+                                },
+                                name: 'col-'+new String(columns.length + 1),
+                                active: false
+                            }
 
-                })
-                .fail(function( jqxhr, settings, exception ) {
-                    console.log("failed to load vue: " + exception);
+                            columns.push(newColumn);
+                            dlg.raw[newColumn.name] = newColumn.data;
+                            dlg.methods.clickTab(columns.length - 1);
+                        }
+                    }
                 });
+
+
+            });
+
 
 
     });
